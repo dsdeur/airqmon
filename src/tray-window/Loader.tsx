6e5f6983ba5e5e9388cb78a79df7a6ac1bc3a939
Jakub Synowiec <github@jakubsynowiec.info>
2018-04-06 23:51:17 +0200

    Refactor Loader component to styled components

--- src/tray-window/Loader.tsx
+++ src/tray-window/Loader.tsx
@@ -1,19 +1,72 @@
 import * as React from 'react';
+import { CenteredContent, ContentSpacing, Text } from '../parts';
+import styled, { IStyleAwareProps, keyframes } from '../styled-components';
 
-const Loader = () => {
+const Loader: React.SFC<IStyleAwareProps> = (props) => {
+  const LoaderSummary = ContentSpacing.extend``;
+
+  const LoaderSpinner = styled.div`
+    margin: auto;
+    width: 50px;
+    height: 40px;
+    text-align: center;
+  `;
+
+  const stretchdelay = keyframes`
+    0%,
+    40%,
+    100% {
+      transform: scaleY(0.4);
+    }
+    20% {
+      transform: scaleY(1);
+    }
+  `;
+
+  const Spinner = styled.div`
+    background-color: ${(props) => props.theme.text.primaryColor};
+    height: 100%;
+    width: 2px;
+    margin: 1px;
+    display: inline-block;
+    animation: ${stretchdelay} 1.2s infinite ease-in-out;
+  `;
+
+  const Spinner1 = Spinner.extend`
+    animation-delay: -1.2s;
+  `;
+
+  const Spinner2 = Spinner.extend`
+    animation-delay: -1.1s;
+  `;
+
+  const Spinner3 = Spinner.extend`
+    animation-delay: -1s;
+  `;
+
+  const Spinner4 = Spinner.extend`
+    animation-delay: -0.9s;
+  `;
+
+  const Spinner5 = Spinner.extend`
+    animation-delay: -0.8s;
+  `;
+
   return (
-    <div className="centered-content">
-      <div className="loader">
-        <div className="loader__summary">Loading data&hellip;</div>
-        <div className="loader__spinner">
-          <div className="loader__spinner__r1" />
-          <div className="loader__spinner__r2" />
-          <div className="loader__spinner__r3" />
-          <div className="loader__spinner__r4" />
-          <div className="loader__spinner__r5" />
-        </div>
+    <CenteredContent>
+      <div className={props.className}>
+        <LoaderSummary>
+          <Text>Loading data&hellip;</Text>
+        </LoaderSummary>
+        <LoaderSpinner>
+          <Spinner1 />
+          <Spinner2 />
+          <Spinner3 />
+          <Spinner4 />
+          <Spinner5 />
+        </LoaderSpinner>
       </div>
-    </div>
+    </CenteredContent>
   );
 };
 
