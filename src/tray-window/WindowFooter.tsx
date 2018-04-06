6a7a2872922fd1cd7f78de921d4f2c9eb916aa4d
Jakub Synowiec <github@jakubsynowiec.info>
2018-04-06 13:48:50 +0200

    Refactor WindowFooter to styled components

--- src/tray-window/WindowFooter.tsx
+++ src/tray-window/WindowFooter.tsx
@@ -1,10 +1,16 @@
-import { ipcRenderer } from 'electron';
+import styled, { IStyleAwareProps } from '../styled-components';
+
 import * as React from 'react';
+
+import { ipcRenderer } from 'electron';
 
 import { formatDateTo24Time } from '../helpers';
 import IPC_EVENTS from '../ipc-events';
 
-interface IWindowFooterProps {
+import Link from './Link';
+import { textMixin } from '../style-mixins';
+
+interface IWindowFooterProps extends IStyleAwareProps {
   lastUpdateDate?: Date;
   isAutoRefreshEnabled: boolean;
   onRefreshClick: () => void;
@@ -12,40 +18,38 @@ interface IWindowFooterProps {
   onQuitClick: () => void;
 }
 
-const WindowFooter = ({
-  lastUpdateDate,
-  isAutoRefreshEnabled,
-  onRefreshClick,
-  onPreferencesClickHandler,
-  onQuitClick,
-}: IWindowFooterProps) => {
+const WindowFooter: React.SFC<IWindowFooterProps> = (props) => {
   function handleExtLinkClick(url: string, event: MouseEvent) {
     event.preventDefault();
     ipcRenderer.send(IPC_EVENTS.OPEN_BROWSER_FOR_URL, url);
   }
 
-  const lastUpdate: string = lastUpdateDate
-    ? `, last update at ${formatDateTo24Time(lastUpdateDate)}`
+  const lastUpdate: string = props.lastUpdateDate
+    ? `, last update at ${formatDateTo24Time(props.lastUpdateDate)}`
     : null;
 
+  const FooterText = styled.div`
+    ${textMixin};
+    margin-top: 10px;
+    padding-left: 10px;
+    font-size: ${parseInt(props.theme.text.smallSize, 10) - 1}px;
+    float: left;
+  `;
+
   return (
-    <footer className="toolbar toolbar-footer">
-      <div className="toolbar-footer__footer-text">
+    <footer className={`toolbar ${props.className}`}>
+      <FooterText>
         Powered by&nbsp;
-        <a
-          className="link"
-          href="#"
-          onClick={handleExtLinkClick.bind(this, 'https://developer.airly.eu')}
-        >
+        <Link href="#" onClick={handleExtLinkClick.bind(this, 'https://developer.airly.eu')}>
           Airly
-        </a>
+        </Link>
         {lastUpdate}
-      </div>
+      </FooterText>
       <div className="toolbar-actions pull-right">
         <div className="btn-group">
           <button
-            className={'btn btn-default' + (isAutoRefreshEnabled ? ' active' : '')}
-            onClick={onRefreshClick}
+            className={'btn btn-default' + (props.isAutoRefreshEnabled ? ' active' : '')}
+            onClick={props.onRefreshClick}
           >
             <span className="icon icon-arrows-ccw" title="Background fetch" />
           </button>
@@ -53,10 +57,10 @@ const WindowFooter = ({
             <span
               className="icon icon-cog"
               title="Preferences"
-              onClick={onPreferencesClickHandler}
+              onClick={props.onPreferencesClickHandler}
             />
           </button>
-          <button className="btn btn-default" onClick={onQuitClick}>
+          <button className="btn btn-default" onClick={props.onQuitClick}>
             <span className="icon icon-cancel" title="Quit" />
           </button>
         </div>
@@ -65,4 +69,8 @@ const WindowFooter = ({
   );
 };
 
-export default WindowFooter;
+const StyledWindowFooter = styled(WindowFooter)`
+  box-shadow: none;
+`;
+
+export default StyledWindowFooter;
