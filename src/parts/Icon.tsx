843e2c6656046ac027f2a1aba0a2141859f40b18
Jakub Synowiec <github@jakubsynowiec.info>
2018-04-06 22:07:54 +0200

    Wrap photonkit icon in a component

---
+++ src/parts/Icon.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import styled, { IStyleAwareProps } from '../styled-components';
+
+enum PhotonIcon {
+  alert = 'alert',
+  direction = 'direction',
+}
+
+interface IIconProps extends IStyleAwareProps {
+  icon: PhotonIcon;
+}
+
+const Icon: React.SFC<IIconProps> = (props) => {
+  return <span className={`icon icon-${props.icon} ${props.className}`} />;
+};
+
+const StyledIcon = styled(Icon)`
+  margin-right: 4px;
+`;
+
+export { PhotonIcon, StyledIcon as Icon };
+export default StyledIcon;
